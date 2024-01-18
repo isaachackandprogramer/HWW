e594a0db1c5eb52c2a25efd274d7788e6212e572
isaachackandprogramer <152911271+isaachackandprogramer@users.noreply.github.com>
2024-01-18 19:24:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,14 @@ print('''
   ##  ##  #######  #######             ##     ##  ##   ##  ##     ##
   ##  ##  #######  #######             ## ##  ##  ##   ##  ##     ##
  ###  ##   ## ##    ## ##               ###    ####     ####     ####''')
+print('Por favor ative o modo administrador pra executar esta ferramenta.')
 
-um = input('1')
+um = input('1-')
 
-if um == "1-":
+if um == "1":
     print('instalando')
-os.system('clear')
+os.system('apt-get update && sudo apt-get upgrade')
+os.system('apt install nmap')
+os.system('apt-get install nikto -y')
+
+print("obrigado por usar só usei pra passar tempo kk")
