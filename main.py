98d835d1798aedc1e639bf45e13e8a2d8e78e21c
isaachackandprogramer <152911271+isaachackandprogramer@users.noreply.github.com>
2024-01-19 00:54:29 -0300

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ print("""\033[1;36m
 3-coding  \033[1;m""")
 print("")
 input1 = input(">")
-if input1 == 1:
+if input1 == '1':
     os.system('apt install nmap')
     os.system('apt install metasploit-framework')    
     os.system('apt-get install hydra-gtk')
@@ -31,9 +31,9 @@ if input1 == 1:
     os.system('apt-get install git')
 
 
-if input1 == 2:
+if input1 == '2':
     os.system('git clone https://github.com/isaachackandprogramer/SGY-TOOL-security')
 
 
-if input1 == 3:
+if input1 == '3':
    os.system('git clone https://github.com/isaachackandprogramer/SGY-TOOL-CODING')
